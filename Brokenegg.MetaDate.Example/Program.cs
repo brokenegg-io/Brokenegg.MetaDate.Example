8f24281bbfdf71aa84cc8e9b53a28f0fd809f0a3
Marco Lima <marckdx@outlook.com>
2021-08-07 20:56:13 -0300

    adding example

---
+++ Brokenegg.MetaDate.Example/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Brokenegg.MetaDate.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = DateTime.ParseExact("25/10/2021 20:25", "dd/MM/yyyy HH:mm", null);
+            USettings.Locale = ULocale.FindLocale(ELocales.MANAUS);
+
+            var uDate = new UDate(date);
+            
+            var local =  uDate.ToLocalDateTime();
+            var utc = uDate.ToUtcDateTime();
+
+            Console.WriteLine(local.ToString("dd/MM/yyyy HH:mm"));
+            Console.WriteLine(utc.ToString("dd/MM/yyyy HH:mm"));
+
+        }
+    }
+}
